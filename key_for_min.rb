f0b7a39f4478c9e53240da5c61529a37410f6a22
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-24 21:01:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+  empty_hash = ""
+  name_hash.collect do |name, value|
+    if value <= 1
+      return name
+      end
+      end
+    empty_hash
+  end
