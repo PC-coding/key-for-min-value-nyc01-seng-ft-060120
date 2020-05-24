b4728a60866d81f1a837de2359cd3fed7945195e
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-24 21:09:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,9 @@ def key_for_min_value(name_hash)
   empty_hash = nil
   smallest_value = 0
   name_hash.collect do |name, value|
-    if value <= 1
-      return name
+    if smallest_value == 0 || value < smallest_value
+      smallest_value = value
+      empty_hash = name
       end
       end
     empty_hash
